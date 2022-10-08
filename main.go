158c0fbde984bd211a3a0b6e8ebdf30a54d63e5e
George Onwuasoanya <georgetheprogrammer@gmail.com>
2022-10-08 15:32:45 +0100

    used repository to return Choices and added http timeout middleware to server.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-kit/log"
 	"github.com/ong-gtp/choice-service/logging"
@@ -13,12 +14,15 @@ import (
 func main() {
 	var logger log.Logger
 	port := "8081"
+	httpTimeout := 60 * time.Second
 	env := "development"
 	logger = log.NewLogfmtLogger(os.Stderr)
 	logger = log.With(logger, "env", env, "listen", port, "caller", log.DefaultCaller)
 
 	svc := logging.NewLoggingMiddleware(logger, service.NewService())
 	r := transport.NewHttpServer(svc, logger)
+	rWithTimeout := http.TimeoutHandler(r, httpTimeout, "Timeout!")
+
 	logger.Log("msg", "HTTP", "addr", port)
-	logger.Log("err", http.ListenAndServe(":"+port, r))
+	logger.Log("err", http.ListenAndServe(":"+port, rWithTimeout))
 }
--- repository/repository.go
+++ repository/repository.go
@@ -13,11 +13,11 @@ func NewRepository() *repository {
 }
 
 var choices = []entities.Choice{
-	{Id: 1, Name: "Rock"},
-	{Id: 2, Name: "Paper"},
-	{Id: 3, Name: "Scissors"},
-	{Id: 4, Name: "Spook"},
-	{Id: 5, Name: "Lizard"},
+	{Id: 1, Name: "rock"},
+	{Id: 2, Name: "paper"},
+	{Id: 3, Name: "scissors"},
+	{Id: 4, Name: "lizard"},
+	{Id: 5, Name: "spock"},
 }
 
 func (r *repository) GetChoices() []entities.Choice {
